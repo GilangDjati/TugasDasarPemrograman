cf92d57cccd37790627bdad43e2f9bd3d37d2833
GilangDjati <97822525+GilangDjati@users.noreply.github.com>
2022-01-16 22:48:58 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    float c;
+    a=10;
+    b=5;
+    c=float(a+b)/2;
+    cout<<"Isi Variabel a = "<<a<<endl;
+    cout<<"Isi Variabel b = "<<b<<endl;
+    cout<<"Isi Variabel c = "<<c<<endl;
+}

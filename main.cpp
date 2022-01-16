1ba1eb8fed4e809c195cc876483a78a9951eca6d
GilangDjati <97822525+GilangDjati@users.noreply.github.com>
2022-01-16 22:52:11 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,13 +4,22 @@ using namespace std;
 
 int main()
 {
-    int panjang, lebar, keliling, luas;
-    panjang = 5;
-    lebar = 7;
-    keliling = 2 * (panjang+lebar);
-    luas = panjang * lebar;
-    cout<<"Program Mencari Keliling dan Luas Persegi Panjang"<<endl;
-    cout<<"================================================="<<endl;
-    cout<<"Keliling = "<<keliling<<endl;
-    cout<<"Luas = "<<luas<<endl;
+// Kamus
+    int n, i, j;
+    string arr_nama_bulan[12]={"Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember"};
+
+// Algoritma
+    cout << "Program Mencetak Nama Bulan Berurutan Sampai Batas ke-n" << endl;
+    cout << "=======================================================" << endl << endl;
+    cout << "Masukkan jumlah bulan = ";
+    cin >> n; cout << endl;
+    for (i=0; i<n; i++)
+        {
+            j=i+1;
+            cout << "Bulan ke-" << j << " = " << arr_nama_bulan[i] << endl;
+        }
+    cout << endl;
+    cout << "=======================================================" << endl;
+    cout << "Program selesai" << endl;
+    return 0;
 }

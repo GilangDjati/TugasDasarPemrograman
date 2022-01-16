0703b9695367d33f7be91da3715fbfedcf845d0e
GilangDjati <97822525+GilangDjati@users.noreply.github.com>
2022-01-16 22:53:17 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,8 +4,18 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    cout << "I’m Learning C++" << endl;
+    float KelilingAlas, Luas, Volume;
+    #define phi 3.14                 // konstanta nilai phi
+    float r;                         // jari-jari tabung
+    float t;                         // tinggi tabung
+    KelilingAlas = 2 * phi * r;
+    Luas = 2 * phi * r * r;
+    Volume = phi * r * r * t;
 
+    cout << "Program Mencari Keliling Alas, Luas, dan Volume Tabung" << endl;
+    cout << "======================================================" << endl;
+    cout << "KelilingAlas = " << KelilingAlas << endl;
+    cout << "Luas = " << Luas << endl;
+    cout << "Volume = " << Volume << endl;
     return 0;
 }
